3afc9f32d35472bdbbed10370b66b4d696d02b6f
EveraldoMiranda <164937028+EveraldoMiranda@users.noreply.github.com>
2024-03-25 22:24:51 -0300

    Update 1-lista.py

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,8 +1,15 @@
 # Crie uma lista apenas com elementos numéricos
+Númerica = [1, 2,3,4]
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
 
+Mista = ['Pyton', 4, [1,2,3,4], True]    
+Len (Mista)
+
+
 # Imprima na tela apenas os 5 primeiros elementos da lista
+
+Print (Mista)
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
 
